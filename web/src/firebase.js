0d0386b6f728f06cedcd32a647bc8a7528f91772
sbrd2010 <roanmaniac@yahoo.com>
2026-05-28 10:55:14 +0200

    Feat: Complete Loci Web ADHD Companion implementation with RTDB sync

---
+++ web/src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKCF2WcJk9kI1YovHBTPrWj2QSdmrjUx0",
+  authDomain: "loci-flow.firebaseapp.com",
+  databaseURL: "https://loci-flow-default-rtdb.firebaseio.com",
+  projectId: "loci-flow",
+  storageBucket: "loci-flow.appspot.com",
+  messagingSenderId: "862993748883",
+  appId: "1:862993748883:web:loci-web-app"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and export
+export const db = getDatabase(app);
