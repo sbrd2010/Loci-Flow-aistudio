9da628dbf506b6d048c7c1dfa0f2d17988847797
sbrd2010 <roanmaniac@yahoo.com>
2026-06-07 17:31:31 +0200

    Make Mind Box router file JSX-free

--- web/src/components/MindBoxTab.js
+++ web/src/components/MindBoxTab.js
@@ -1,11 +1,1 @@
-import DeadlineProgressMirror from "./DeadlineProgressMirror.jsx";
-import MindBoxTabBase from "./MindBoxTab.jsx";
-
-export default function MindBoxTab(props) {
-  return (
-    <>
-      <DeadlineProgressMirror payload={props.payload} />
-      <MindBoxTabBase {...props} />
-    </>
-  );
-}
+export { default } from "./MindBoxTabWithProgressMirror.jsx";
